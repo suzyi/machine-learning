89a459cd3d77e34f49172d43bdbc8f05f3ba0e8b
suzyi <1274865991@qq.com>
2022-04-09 16:30:52 +0800

    Rename some variables to make them suit most cases.

--- py-files/plot_hist.py
+++ py-files/plot_hist.py
@@ -1,28 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_error_hist(temporature_Jul, temporature_Jan, fileName="hist.png"):
+def plot_error_hist(vec_1, vec_2, fileName="hist.png"):
     """
     inputs:
-        temporature_Jul (list): A list float values.
-        temporature_Jan (list): A list float values.
+        vec_1 (list): A list float values.
+        vec_2 (list): A list float values.
     outputs:
         
     """
     thresh = 40.0
-    kwargs = dict(alpha=0.3, bins=10) # alpha controls transparency
-    plt.hist([e for e in temporature_Jul if e<thresh], **kwargs, color='r', label=f'temporature(July, {len(temporature_Jul)})')
-    plt.hist(temporature_Jan, **kwargs, color='g', label=f'temporature(January, {len(temporature_Jan)})')
-    plt.gca().set(xlabel='temporature(Celsius)', ylabel='count') # gca stands for "get current axes"
+    kwargs = dict(alpha=0.3, bins=30) # alpha controls transparency
+    plt.hist([e for e in vec_1 if e<thresh], **kwargs, color='r', label=f'length of vec_1({len(vec_1)})')
+    plt.hist(vec_2, **kwargs, color='g', label=f'length of vec_2({len(vec_2)})')
+    plt.gca().set(xlabel='length', ylabel='count') # gca stands for "get current axes"
     plt.legend()
-    plt.savefig(f"results/{fileName}")
+    plt.savefig(f"{fileName}")
     plt.close()
     
 if __name__=="__main__":
     sigma = 1
-    mu_Jul = 29
-    temporature_Jul = np.random.normal(mu_Jul, sigma, 30).tolist()
-    temporature_Jul.append(44) # append an outlier
-    mu_Jan = 4
-    temporature_Jan = np.random.normal(mu_Jan, sigma, 31).tolist()
-    plot_error_hist(temporature_Jul, temporature_Jan, fileName="hist.png")
+    mu_1 = 29
+    vec_1 = np.random.normal(mu_1, sigma, 30).tolist()
+    vec_1.append(44) # append an outlier
+    mu_2 = 4
+    vec_2 = np.random.normal(mu_2, sigma, 31).tolist()
+    plot_error_hist(vec_1, vec_2, fileName="hist.png")
